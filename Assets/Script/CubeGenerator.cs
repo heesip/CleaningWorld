73fafb25a6af6d8b161256c3a4acf037ffcab87d
HeeSeop Kim <khs37705@naver.com>
2022-07-31 00:54:17 +0900

    큐브제네레이터 추가

---
+++ Assets/Script/CubeGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeGenerator : MonoBehaviour
+{
+    [SerializeField] Transform cubePrefab;
+
+    private IEnumerator Start()
+    {
+        Debug.Assert(cubePrefab != null, "큐브가 없습니다!!");
+        for (int i = 0; i < 5; i++)
+        {
+            Instantiate(cubePrefab,
+                        Vector3.zero + Vector3.right * i,
+                        Quaternion.identity);
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
