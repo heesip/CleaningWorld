4ae5ddc1a86f8ca758ae5d47d60625c38f2d6f76
HeeSeop Kim <khs37705@naver.com>
2022-07-31 23:11:06 +0900

    1. 조이스틱 패키지 설치 2. 플레이어 무브시스템 추가 3. 플레이어 연동

--- Assets/_Script/Player/Player.cs
+++ Assets/_Script/Player/Player.cs
@@ -1,18 +1,17 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] PlayerMoveSystem playerMoveSystem = new PlayerMoveSystem();
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        playerMoveSystem.Initalize(this);
+    }
+    private void FixedUpdate()
+    {
+        playerMoveSystem.Move();
     }
 }
---
+++ Assets/_Script/Player/PlayerMoveSystem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerMoveSystem
+{
+    Joystick joystick;
+    Player player;
+
+    [SerializeField] float speed = 5f;
+    Vector3 direction;
+    Vector3 moveResult;
+
+    public void Initalize(Player player)
+    {
+        this.player = player;
+    }
+
+    public void Move()
+    {
+        //얼리리턴 방식
+        if (joystick.IsDrag == false)
+        {
+            return;
+        }
+
+        // 캐릭터 움직임 작성
+
+        direction.x = joystick.Horizontal;
+        direction.z = joystick.Vertical;
+        direction.Normalize();
+
+        moveResult = speed * Time.deltaTime * direction;
+        player.transform.Translate(moveResult);
+    }
+
+}
