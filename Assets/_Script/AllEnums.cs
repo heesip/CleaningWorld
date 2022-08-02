a1c89be971bff18b4fece35435d327afdefd65c5
HeeSeop Kim <khs37705@naver.com>
2022-08-03 00:15:24 +0900

    1. 불필요한 스크립트 제거 2. AllEums 추가 3. AllTagAndLayer 추가

---
+++ Assets/_Script/AllEnums.cs
@@ -0,0 +1,19 @@
+﻿    public enum GarbageType
+{
+    None,
+
+    Can1,
+    Can2, 
+
+    Food1, 
+    Food2,
+    
+    Glass1,
+    Glass2,
+
+    Plastic1,
+    Plastic2,
+
+    Paper1,
+    Paper2
+}
--- Assets/_Script/Manager/FactoryManager.cs
+++ Assets/_Script/Manager/FactoryManager.cs
@@ -4,17 +4,8 @@ using UnityEngine;
 
 public class FactoryManager : Singleton<FactoryManager>
 {
-    ObjectPoolSystem cubeObjectPool;
-    [SerializeField] TestCube cubePrefab;
-
     public void Initalize()
     {
-        cubeObjectPool = new ObjectPoolSystem(cubePrefab, 5, transform);
-    }
 
-    public TestCube GetTestCube()
-    {
-        return cubeObjectPool.Get() as TestCube;
     }
-
 }
