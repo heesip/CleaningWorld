1db049ad47632ebba0b69449af69b9f6a01a31db
HeeSeop Kim <khs37705@naver.com>
2022-08-07 21:44:18 +0900

    PrafabResourceInfo 생성

---
+++ Assets/_ScriptableObject/Scripts/PrefabResourcesInfoSO.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = nameof(PrefabResourcesInfoSO), menuName = "CleaningWorld/Create PrefabResourcesInfoSO")]
+public class PrefabResourcesInfoSO : ScriptableObject
+{
+    [SerializeField] GarbageObject canPrefab1;
+    [SerializeField] GarbageObject canPrefab2;
+    [SerializeField] GarbageObject foodPrefab1;
+    [SerializeField] GarbageObject foodPrefab2;
+    [SerializeField] GarbageObject glassPrefab1;
+    [SerializeField] GarbageObject glassPrefab2;
+    [SerializeField] GarbageObject paperPrefab1;
+    [SerializeField] GarbageObject paperPrefab2;
+    [SerializeField] GarbageObject plasticPrefab1;
+    [SerializeField] GarbageObject plasticPrefab2;
+
+
+}
