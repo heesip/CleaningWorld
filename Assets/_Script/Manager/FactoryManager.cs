4a523aff0757b230ea10725d6eb96726d4ca12b4
HeeSeop Kim <khs37705@naver.com>
2022-08-08 00:24:37 +0900

    FactoryManager GetGarbageObject추가

--- Assets/_Script/Manager/FactoryManager.cs
+++ Assets/_Script/Manager/FactoryManager.cs
@@ -6,9 +6,14 @@ using UnityEngine;
 public class FactoryManager : Singleton<FactoryManager>
 {
     PrefabFactorySystem prefabFactorySystem = new PrefabFactorySystem();
-    
+
     public void Initialize()
     {
         prefabFactorySystem.Initialize(transform);
+    }
+
+    public GarbageObject GetGarbageObject(GarbageType garbageType, Vector3 spawnPoint)
+    {
+        return prefabFactorySystem.GetGarbageObject(garbageType, spawnPoint);
     }
 }
