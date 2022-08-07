64abef5414e6d780b1153421a00e21fea230555f
HeeSeop Kim <khs37705@naver.com>
2022-08-08 01:02:52 +0900

    PlayerDectector 추가 GarbageHeapPlayerDetector 추가

---
+++ Assets/_Script/Garbage/GarbageHeapPlayerDetector.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarbageHeapPlayerDetector : PlayerDetector
+{
+    protected override void _onTriggerEnter()
+    {
+        Debug.Log("미치겠다~"); 
+    }
+
+    protected override void _onTriggerExit() { }
+
+}
